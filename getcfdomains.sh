83ec9f6e7621d0a956036cbc44cfdbc89299c06a
Rhea Rodriguez <rhea_rod_888@yahoo.com>
2022-10-21 22:04:06 +0800

    add bash

---
+++ getcfdomains.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ curl -X GET "https://api.cloudflare.com/client/v4/zones?page=1&per_page=50&account.id=f7cc5dfdfdf9e4cfeaebab03115f25e&status=active" -H "Authorization: Bearer dfarujfurjfurjfurjve" \ -H "Content-Type: application/json" | jq '.result[] | "\(.id) \(.name)"' >> listdomains.txt
